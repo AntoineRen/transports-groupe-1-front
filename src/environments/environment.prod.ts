ba0d818ee92c289e72225063a3210cf6ca5e2477
Antoine Renou <antoine.renou.pro@gmail.com>
2020-05-26 12:52:29 +0200

    http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { config } from './common';
 
 export const environment = Object.assign({
   production: true,
-  baseUrl: 'http://app-3f65a3f7-b7de-44d2-89fc-0812aee2f9fa.cleverapps.io/'
+  baseUrl: 'https://app-3f65a3f7-b7de-44d2-89fc-0812aee2f9fa.cleverapps.io/'
 }, config);
